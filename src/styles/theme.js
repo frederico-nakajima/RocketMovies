7cb52d0eac7aef079d74b4f94226eac233fe969f
frederico <fredericonakajimanakajima@gmail.comm>
2024-06-21 01:03:31 -0300

    theme,rota

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,7 +3,8 @@ export default {
       BACKGROUND_900: "#262529",
       BACKGROUND_800: "#1C1B1E",
       BACKGROUND_700: "#3E3B47",
-  
+      BACKGROUND_BROWN:"rgba(255, 133, 155, 0.05)",
+      
       WHITE: "#F4EDE8",
       PINK: "#FF859B",
   
